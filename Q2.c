53bfd928f076aa59007f6cef2ee695970b2b4e31
Tamana517 <tamana7522@gmail.com>
2025-08-26 23:37:36 +0530

    Update Q2.c

--- Q2.c
+++ Q2.c
@@ -5,47 +5,77 @@ Q2: Given a string, reverse it using STACK. For example “DataStructure” shou
 
 #include <stdio.h>
 #include <string.h>
-#define MAX 100
+#define MAX 100  // max string length
 
-char stack[MAX];
-int top = -1;
+// stack struct for characters
+typedef struct {
+    char items[MAX];  // stack elements
+    int top;          // index of top
+} Stack;
 
-// Push to stack
-void push(char c) {
-    if (top < MAX - 1)
-        stack[++top] = c;
-    else
-        printf("Stack Overflow\n");
+// initialize stack
+Stack init() {
+    Stack s;
+    s.top = -1;
+    return s;
 }
 
-// Pop from stack
-char pop() {
-    if (top >= 0)
-        return stack[top--];
+// check empty
+int isEmpty(Stack s) {
+    return s.top == -1;
+}
+
+// check full
+int isFull(Stack s) {
+    return s.top == MAX - 1;
+}
+
+// push character
+Stack push(Stack s, char ch) {
+    if (isFull(s))
+        printf("Stack Overflow!\n");
+    else
+        s.items[++s.top] = ch;
+    return s;
+}
+
+// pop character
+Stack pop(Stack s, char *ch) {
+    if (isEmpty(s))
+        printf("Stack Underflow!\n");
     else {
-        printf("Stack Underflow\n");
-        return '\0';
+        *ch = s.items[s.top];
+        s.top--;
+    }
+    return s;
+}
+
+// reverse string using stack
+void reverseString(char str[]) {
+    Stack s = init();
+    int len = strlen(str);
+    char ch;
+
+    // push all characters
+    for (int i = 0; i < len; i++)
+        s = push(s, str[i]);
+
+    // pop characters back into string
+    for (int i = 0; i < len; i++) {
+        s = pop(s, &ch);
+        str[i] = ch;
     }
 }
 
 int main() {
     char str[MAX];
-    int i, len;
-
+    
     printf("Enter a string: ");
-    scanf(" %[^\n]", str);   // input with spaces
+    scanf("%s", str);
 
-    len = strlen(str);
+    reverseString(str);
 
-    // Push all characters
-    for (i = 0; i < len; i++)
-        push(str[i]);
-
-    // Pop and print reversed
-    printf("Reversed: ");
-    for (i = 0; i < len; i++)
-        printf("%c", pop());
-    printf("\n");
+    printf("Reversed string: %s\n", str);
 
     return 0;
 }
