761f7a93ab380f835bf26266ae7a4934b300a2a6
Tamana517 <tamana7522@gmail.com>
2025-08-26 23:54:55 +0530

    Createe Q5.c

--- Q5.c
+++ Q5.c
@@ -1,64 +1,99 @@
 // Q5: Write a program for the evaluation of a Postfix expression.
 
-#include <stdio.h>
-#include <ctype.h>  // for isdigit()
+// Q5: Program to evaluate a Postfix expression (supports multi-digit operands)
 
+#include <stdio.h>
+#include <stdlib.h>   // for atoi()
+#include <string.h>   // for strtok()
+#include <ctype.h>    // for isdigit()
+#include <math.h>     // for pow()
 #define MAX 100
 
-int stack[MAX];
-int top = -1;
+// Define stack
+typedef struct {
+    int arr[MAX];
+    int top;
+} stack;
 
-// Push value onto stack
-void push(int val) {
-    if (top < MAX - 1) {
-        stack[++top] = val;
-    }
+// Initialize stack
+stack init() {
+    stack S;
+    S.top = -1;
+    return S;
 }
 
-// Pop value from stack
-int pop() {
-    if (top >= 0) {
-        return stack[top--];
-    }
-    return -1; // empty stack case
+// Check empty
+int isEmpty(stack S) {
+    return (S.top == -1);
 }
 
-// Evaluate a postfix expression
+// Check full
+int isFull(stack S) {
+    return (S.top == MAX - 1);
+}
+
+// Push
+stack push(stack S, int x) {
+    if (isFull(S))
+        printf("Stack Overflow!\n");
+    else
+        S.arr[++S.top] = x;
+    return S;
+}
+
+// Pop
+stack pop(stack S, int *x) {
+    if (isEmpty(S))
+        printf("Stack Underflow!\n");
+    else
+        *x = S.arr[S.top--];
+    return S;
+}
+
+// Evaluate Postfix expression
 int evaluatePostfix(char exp[]) {
-    for (int i = 0; exp[i] != '\0'; i++) {
-        char c = exp[i];
+    stack S = init();
+    int op1, op2, result;
+    char *token = strtok(exp, " ");  // split by spaces
 
-        // If operand (digit), push on stack
-        if (isdigit(c)) {
-            push(c - '0'); // convert char to int
+    while (token != NULL) {
+        if (isdigit(token[0])) {  
+            // if number, convert string → int
+            int num = atoi(token);
+            S = push(S, num);
         }
-        // If operator, pop two operands and apply
-        else {
-            int val2 = pop();
-            int val1 = pop();
+        else {  
+            // operator → pop two operands
+            S = pop(S, &op2);
+            S = pop(S, &op1);
 
-            switch (c) {
-                case '+': push(val1 + val2); break;
-                case '-': push(val1 - val2); break;
-                case '*': push(val1 * val2); break;
-                case '/': push(val1 / val2); break;
+            switch (token[0]) {
+                case '+': result = op1 + op2; break;
+                case '-': result = op1 - op2; break;
+                case '*': result = op1 * op2; break;
+                case '/': result = op1 / op2; break;
+                case '^': result = pow(op1, op2); break;
+                default:  result = 0;
             }
+            S = push(S, result);
         }
+        token = strtok(NULL, " ");  // next token
     }
-    // Final result remains on stack
-    return pop();
+
+    S = pop(S, &result);  // final result
+    return result;
 }
 
+// Main
 int main() {
     char exp[MAX];
 
-    // Input expression
-    printf("Enter postfix expression (e.g., 23*54*+9-): ");
-    scanf("%s", exp);
+    printf("----- Postfix Evaluation -----\n");
+    printf("Enter postfix expression (use spaces, e.g. \"12 3 + 4 *\"):\n");
+    fgets(exp, MAX, stdin);  // read full line
 
-    // Evaluate and display result
     int result = evaluatePostfix(exp);
-    printf("Result = %d\n", result);
 
+    printf("Result = %d\n", result);
     return 0;
 }
