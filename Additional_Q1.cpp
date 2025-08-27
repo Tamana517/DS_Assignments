1fe141d1edcb76b2c3b598344cd65527ccc5cc70
Tamana517 <tamana7522@gmail.com>
2025-08-27 15:41:21 +0530

    Create Additional_Q1.cpp

--- Additional_Q1.cpp
+++ Additional_Q1.cpp
@@ -1,5 +1,5 @@
 /*
-Given an array A, find the nearest smaller element for every element A[i] in the array such that the
+Q1: Given an array A, find the nearest smaller element for every element A[i] in the array such that the
 element has an index smaller than i.
 */
 
