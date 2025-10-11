e6f0774e3f20a0c8a84017b4e8e71f1fc6fa2c05
Tamana517 <tamana7522@gmail.com>
2025-10-11 20:37:10 +0530

    Create Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,5 +1,5 @@
 /*
-Display all the node values in a circular linked list, repeating value of head node at the
+Q2: Display all the node values in a circular linked list, repeating value of head node at the
 end too.
 Input: 20 → 100 → 40 → 80 → 60,
 Output: 20 100 40 80 60 20.
